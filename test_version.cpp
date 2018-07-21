b7617f939d74d91c0ee6f69c2d1387b540562fb6
Nikita Dudin <Nikenja@yandex.com>
2018-07-21 11:20:47 +0500

    Change test again

--- test_version.cpp
+++ test_version.cpp
@@ -8,7 +8,7 @@ BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version) {
     int version = boost::lexical_cast<int>(Version::getVersion());
-    BOOST_CHECK(!version.empty());
+    BOOST_CHECK(version == 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
