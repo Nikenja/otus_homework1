7ac88455ccd415153b2bf3bf2ae7e85b1ff7eb8d
Nikita Dudin <Nikenja@yandex.com>
2018-09-25 18:26:32 +0500

    From work

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <boost/program_options.hpp>
 #include "versioner.h"
 
 using namespace std;
 
-int main() {
+namespace options = boost::program_options;
+
+int main(int argc, char** argv) {
+    try {
+        options::option_description desc("Allowed options");
+        // desc.add_options()("help", "produce help message");
+    } catch(...) {
+
+    }
     cout << "Hello, World!" << endl;
+
     cout << "Version: " << Versioner::getVersion() << endl;
     cout << "Revision: " << Versioner::getRevision() << endl;
     return 0;
