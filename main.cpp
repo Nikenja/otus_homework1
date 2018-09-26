6601158c8e869613b1c619e80ff8e5eb4722f1db
Nikita Dudin <Nikenja@yandex.com>
2018-09-26 13:11:01 +0500

    Add program options

--- main.cpp
+++ main.cpp
@@ -1,21 +1,37 @@
+#include <string>
 #include <iostream>
 #include <boost/program_options.hpp>
 #include "versioner.h"
 
 using namespace std;
 
-namespace options = boost::program_options;
+namespace po = boost::program_options;
 
 int main(int argc, char** argv) {
     try {
-        options::option_description desc("Allowed options");
-        // desc.add_options()("help", "produce help message");
-    } catch(...) {
+        po::options_description desc("Allowed options");
+        desc.add_options()
+            ("help", "produce help message")
+            ("version", "print program version, revision and exit");
+        po::variables_map options;
+        po::store(po::parse_command_line(argc, argv, desc), options);
+        po::notify(options);
 
+        if(options.count("help")) {
+            cout << desc << endl;
+            return 1;
+        }
+        if(options.count("version")) {
+            cout << "version: " << Versioner::getVersion() << endl;
+            cout << "revision: " << Versioner::getRevision() << endl;
+            return 1;
+        }
+    } catch(const po::unknown_option& error) {
+        cout << error.what() << endl;
+        return 1;
     }
-    cout << "Hello, World!" << endl;
 
-    cout << "Version: " << Versioner::getVersion() << endl;
-    cout << "Revision: " << Versioner::getRevision() << endl;
+    cout << "Hello, World!" << endl;
+    
     return 0;
 }
